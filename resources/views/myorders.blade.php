0fb2ef8300b8cf44e5e95192b9a38089819e94d9
Ashwinder <ashwinder87@gmail.com>
2021-04-12 20:53:48 +0530

    registration done & project Done

--- resources/views/myorders.blade.php
+++ resources/views/myorders.blade.php
@@ -4,11 +4,11 @@
     
     <div class="col-sm-10">
         <div class="trending-wrapper">
-            <h4>Result for Products</h4>
-            <a class="btn btn-success" href="ordernow">Order Now</a><br><br>
+            <h4>My Orders</h4>
+            
 
             <div class="carousel-inner">
-                @foreach ($products as $item)
+                @foreach ($orders as $item)
                   <div class=" row searched-item cart-list-dividor">
                   <div class="col-sm-3">
                     <a href="detail/{{$item->id}}">
@@ -18,20 +18,21 @@
                   <div class="col-sm-4">
                    
                             <div class="">
-                            <h2>{{$item->name}}</h2>
-                            <h5>{{$item->description}}</h5>
+                            <h2>Name: {{$item->name}}</h2>
+                            <h5>Delivery Status: {{$item->status}}</h5>
+                            <h5>Address: {{$item->address}}</h5>
+                            <h5>Payment Status: {{$item->payment_status}}</h5>
+                            <h5>Payment Method: {{$item->payment_method}}</h5>
                             
                             </div>
                           
                   </div>
-                  <div class="col-sm-3">
-                   <a href="/removecart/{{$item->cart_id}}"class="btn btn-warning">Remove to Cart</a>
-                  </div>
+                  
                 </div>
                     
                 @endforeach
               </div>
-              <a class="btn btn-success" href="ordernow">Order Now</a><br><br>
+              
         
         </div>  
 
